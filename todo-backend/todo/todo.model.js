a97297c8b8acf328058b6d21d444be72907659f4
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-01-17 22:37:50 +0200

    timestamps added to todo model

--- todo-backend/todo/todo.model.js
+++ todo-backend/todo/todo.model.js
@@ -1,27 +1,32 @@
 import mongoose from "mongoose";
 
-const todoSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-    maxLength: 20,
+const todoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 20,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 55,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
   },
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-    maxLength: 55,
-  },
-  date: {
-    type: Date,
-    required: true,
-  },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "User",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const Todo = mongoose.model("Todo", todoSchema);
