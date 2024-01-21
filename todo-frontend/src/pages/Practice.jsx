2fd0f84c6ef44ab7f95f59fd5de07d3bc083f84c
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-01-21 23:51:21 +0200

    useEffect and axios

--- todo-frontend/src/pages/Practice.jsx
+++ todo-frontend/src/pages/Practice.jsx
@@ -1,29 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const NewComponent = () => {
-  const animals = ["tiger", "cow", "elephant", "dog", "cat", "horse"];
+  const [selectedId, setSelectedId] = useState(1);
 
   return (
     <div>
-      <h3>Animal list</h3>
-      <List animals={animals} />
+      <button
+        onClick={() => {
+          setSelectedId(1);
+        }}
+      >
+        1
+      </button>
+      <button
+        onClick={() => {
+          setSelectedId(2);
+        }}
+      >
+        2
+      </button>
+      <button
+        onClick={() => {
+          setSelectedId(3);
+        }}
+      >
+        3
+      </button>
+      <button
+        onClick={() => {
+          setSelectedId(4);
+        }}
+      >
+        4
+      </button>
+      <Todo id={selectedId} />
     </div>
   );
 };
 
-function List({ animals }) {
-  if (!animals) {
-    return <div>...Loading</div>;
-  }
-  if (animals.length === 0) {
-    return <div>There are no animals</div>;
-  }
+function Todo({ id }) {
+  const [todo, setTodo] = useState({});
+
+  // useEffect(() => {
+  //   axios
+  //     .get(`https://sum-server.100xdevs.com/todo?id=${id}`)
+  //     .then((response) => {
+  //       setTodo(response.data.todo);
+  //     });
+  // }, [id]);
+
+  //if you want the api to render after 5 seconds after clicking the button, you can simply put the axios inside a serTimeout Function
+  setTimeout(() => {
+    axios
+      .get(`https://sum-server.100xdevs.com/todo?id=${id}`)
+      .then((response) => {
+        setTodo(response.data.todo);
+      });
+  }, 5000);
   return (
-    <ul>
-      {animals.map((animal) => {
-        return <li key={animal}>{animal}</li>;
-      })}
-    </ul>
+    <>
+      <h1>{todo.title}</h1>
+      <h4>{todo.description}</h4>
+    </>
   );
 }
 
